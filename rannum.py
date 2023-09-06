17a81711bef07486fd4f5eb5c0584667d42a643d
Nastya_J <winkos95@mail.ru>
2023-09-06 19:09:02 +0500

    first commit

---
+++ rannum.py
@@ -0,0 +1,57 @@
+import random
+import string
+from tkinter import *
+
+
+root = Tk()
+root.geometry('400x280')
+root.title('Password generate')
+
+title = StringVar()
+label = Label(root, textvariable=title).pack()
+title.set('The strength of password:')
+
+def selection():
+    selection = choice.get()
+    
+choice = IntVar()
+R1 = Radiobutton(root, text='POOR', variable=choice, value=1, command=selection).pack(anchor=CENTER)
+R2 = Radiobutton(root, text='AVERAGE', variable=choice, value=2, command=selection).pack(anchor=CENTER)
+Radiobutton(root, text='ADVACED', variable=choice, value=2, command=selection).pack(anchor=CENTER)
+
+labelchoice = Label(root)
+labelchoice.pack()
+
+lenlabel = StringVar()
+lenlabel.set('password lenth')
+lentitle = Label(root, textvariable=lenlabel).pack()
+
+val = IntVar()
+spinlenght = Spinbox(root, from_=8, to_=24, textvariable=val, width=13).pack()
+
+def callback():
+    lsum.config(text= passgen())
+
+passgenButton = Button(root, text='Generate password', bd=5, height=2, command=callback)
+passgenButton.pack()
+password = str(callback)
+
+lsum = Label(root, text='')
+lsum.pack(side=BOTTOM)
+
+poor = string.ascii_lowercase + string.ascii_uppercase
+average = string.ascii_uppercase + string.ascii_lowercase + string.digits
+symbols = '''~!@#$%^&*()_+={}[]/:<>?'''
+advance = poor + average + symbols
+
+
+
+def passgen():
+    if choice.get() == 1:
+        return ''.join(random.sample(poor, val.get()))
+    elif choice.get() == 2:
+        return ''.join(random.sample(average, val.get()))
+    elif choice.get() == 3:
+        return ''.join(random.sample(advance, val.get()))
+
+root.mainloop()
